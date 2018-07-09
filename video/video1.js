173820cfd8b139a058a66448b2fd4b0a25e53951
Shradha Kaldate <shradha.kaldate@gmail.com>
2018-07-09 01:09:31 -0400

    Update video1.js

--- video/video1.js
+++ video/video1.js
@@ -2,6 +2,7 @@
   const videoElement = document.getElementById("video"); //video element created in index.html file
   let videoStream;
 
+//replace this code in step 3 
   navigator.mediaDevices.getUserMedia({ video: { facingMode: 'user' } }) //get user media, select video
     .then(stream => { //then get the stream of the video
       videoStream = stream; //store the stream in videostream 
