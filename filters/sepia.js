b3d0421290d4ba594a9c085fcc40ccf9a2da17fb
Shradha Kaldate <shradha.kaldate@gmail.com>
2018-07-18 16:32:59 -0400

    Update sepia.js

--- filters/sepia.js
+++ filters/sepia.js
@@ -1,20 +1,31 @@
-$("#sepia").change(function(){
-    let sepiaVal = $(this).val();
-    let filterVal =  'sepia('+ sepiaVal + ')';
-    let currentFilters = $("#video").css('filter');
-    let filtersToApply = ""
+//when input with id sepia changes
+    $("#sepia").change(function() {
+      //get the current value of the sepia slider
+      let sepiaVal = $(this).val();
+      //create the value we want to apply 
+      let filterVal =  'sepia('+ sepiaVal + ')';
+      //get the current filters applied to video
+      let currentFilters = $("#video").css('filter');
+      let filtersToApply = ""
+      
+      //determine is sepia filter is already applied
+      
+      //if it is not applied we want to create a filter
+      let sepialFilterApplied  = currentFilters.indexOf("sepia(") >= 0;
     
-    let sepialFilterApplied  = currentFilters.indexOf("sepia(") >= 0;
-   
-    if(sepialFilterApplied){
-      filtersToApply = currentFilters.replace(/sepia\(\d.?\d?\)/g, filterVal);
-    }
-    else{
-      filtersToApply = currentFilters == "none"? filterVal : currentFilters + filterVal;
-    }
+      //if sepia filter is applied we want to replace it
+      if(sepialFilterApplied){
+        filtersToApply = currentFilters.replace(/sepia\(\d.?\d?\)/g, filterVal);
+      }
+      //if it is not applied we want to create a filter
+      else{
+        filtersToApply = currentFilters == "none"? filterVal : currentFilters + filterVal;
+      }
+      
+      //apply the filter to the video
+      $("#video").css('filter',filtersToApply)
+                .css("-webkit-filter", filtersToApply);
     
-    $("#video").css('filter',filtersToApply)
-               .css("-webkit-filter", filtersToApply);
-  
-     context.filter = filtersToApply;
+      //apply the filter to the canvas
+      context.filter = filtersToApply;
   })
