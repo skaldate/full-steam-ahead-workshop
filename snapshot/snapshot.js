57a10ebe75d713260c2ed3ffab39d5d5880c8e80
Shradha Kaldate <shradha.kaldate@gmail.com>
2018-07-18 16:20:44 -0400

    Update snapshot.js

--- snapshot/snapshot.js
+++ snapshot/snapshot.js
@@ -1,13 +1,19 @@
-// get canvas element we created in index.html
+  // get canvas element we created in index.html
   let canvas=document.querySelector('canvas');
   // get 2d context of the canavs.
   // it provides function to draw 2d images on canvas
   let context=canvas.getContext('2d');
   
+  
   //when button with id snap is clicked
   $("#snap").click(function(){
-   //draw the image on canvas
-   //image to draw is in element with id video
-   
+    //set the width of canvas to be same as width of video
+    canvas.width = $("#video").width();
+    
+    //set the height of canvas to be same as height of video
+    canvas.height = $("#video").height();
+    
+    //draw the image on canvas
+    //image to draw is in element with id video
     context.drawImage(video,0,0,canvas.width,canvas.height);
   });
