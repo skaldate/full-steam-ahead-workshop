4f5669daeaa5ed22275c994929d42bfd2f2cd4a1
Shradha Kaldate <shradha.kaldate@gmail.com>
2018-07-18 15:11:31 -0400

    Update video1.js

--- video/video1.js
+++ video/video1.js
@@ -1,12 +1,13 @@
 
-  //get the video element created in index.html file and store it in a variable
-  const videoElement = document.getElementById("video"); 
+ //get the video element created in index.html file
+    const videoElement = document.getElementById("video"); 
 
-  navigator.mediaDevices.getUserMedia({ video: { facingMode: 'user' } }) //get media on users device, select video
-    .then(stream => { //then get the video stream from device's video camera
-      //assign the stream to video html element's src object. 
-      //This will stream video from camera to the browser
-      videoElement.srcObject = videoStream; 
-      //play the video
-      videoElement.play(); 
+     //get media on users device, select video
+     //then get the video stream from device's video camera 
+    navigator.mediaDevices.getUserMedia({ video: { facingMode: 'user' } })
+      .then(stream => {       
+        //assign the stream to video html element's src object. 
+        videoElement.srcObject = videoStream; 
+        //play the video
+        videoElement.play(); 
    })
