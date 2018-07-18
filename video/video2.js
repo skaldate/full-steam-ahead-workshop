ab030025b1cab63d445c9aad68a37712f386d7be
Shradha Kaldate <shradha.kaldate@gmail.com>
2018-07-18 15:25:47 -0400

    Update video2.js

--- video/video2.js
+++ video/video2.js
@@ -1,21 +1,32 @@
 $(document).ready(function(){
-  console.log("Video script loaded");
+  //get the video element created in index.html file
   const videoElement = document.getElementById("video");
   let videoStream;
 
-  $("#play").click(function(){
+  //when button with id start is clicked
+  $("#start").click(function(){
+      //get media on users device, select video
       navigator.mediaDevices.getUserMedia(
+        //then get the video stream from device's video camera 
         { video: { facingMode: 'user' }
       })
       .then(stream => {
+        //assign the stream to video html element's src object. 
         videoElement.srcObject = stream;
+        //save the stream in variable videostream 
+        //we want to save it so that we can close it later
         videoStream = stream;
+        //play the video
         videoElement.play(); 
       });
   })
 
+  //when button with id stop is clicked
   $("#stop").click(function(){
+    //if the video is playing
+    //i:e videoStream is not empty.
     if(videoStream != undefined){
+      //stop the video
       videoStream.getTracks()[0].stop();
     }
   })
