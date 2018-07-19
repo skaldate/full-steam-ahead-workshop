8c62e02524f51d02226fb53d37e267e22c0cb4d1
Shradha Kaldate <shradha.kaldate@gmail.com>
2018-07-18 21:04:37 -0400

    Update blur.js

--- filters/blur.js
+++ filters/blur.js
@@ -1,22 +1,30 @@
 $("#blur").change(function(){
-    let blurVal = $(this).val();
-    let filterVal =  'blur('+ blurVal + 'px)';
-    let filters = $("#video").css('filter');
+      //get the current value of the blur slider
+      let blurVal = $(this).val();
+      //create the value we want to apply 
+      let filterVal =  'blur('+ blurVal + 'px)';
+      //get the current filters applied to video
+      let currentFilters = $("#video").css('filter');
+      let filtersToApply = ""
     
-    console.log("Filters :"  +filters);
-    let blurFilterApplied  = filters.indexOf("blur(") >= 0;
-    if(filters == "none"){
-      filters = ""
-    }
-    if(blurFilterApplied){
-      filters = filters.replace(/blur\(\dpx\)/g, filterVal)
-    }
-    else{
-      filters = filters+ filterVal;
-    }
-    console.log(filters);
-    $("#video").css('filter',filters)
-               .css("-webkit-filter", filters);
-  
-     context.filter = filters;
+      //determine is blur  filter is already applied
+      let blurFilterApplied  = filters.indexOf("blur(") >= 0;
+      if(currentFilters == "none"){
+        currentFilters = "";
+      }
+      
+      //if blur filter is already applied we want to replace it
+      if(blurFilterApplied){
+        filtersToApply = currentFilters.replace(/blur\(\dpx\)/g, filterVal)
+      }
+      //if it is not applied we want to create a filter
+      else{
+        filtersToApply = currentFilters+ filterVal;
+      }
+      
+      //apply the filter to the video
+      $("#video").css('filter',filtersToApply)
+                .css("-webkit-filter", filtersToApply);
+      //apply the filter to the video
+      context.filter = filtersToApply;
   })
