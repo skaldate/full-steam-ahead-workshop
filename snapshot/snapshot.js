023f0bd6b52aa7e4cff6599ef59e385587bf5550
Shradha Kaldate <shradha.kaldate@gmail.com>
2018-07-19 00:09:11 -0400

    Update snapshot.js

--- snapshot/snapshot.js
+++ snapshot/snapshot.js
@@ -1,4 +1,4 @@
-  // get canvas element we created in index.html
+// get canvas element we created in index.html
   let canvas=document.querySelector('canvas');
   // get 2d context of the canavs.
   // it provides function to draw 2d images on canvas
@@ -12,8 +12,15 @@
     
     //set the height of canvas to be same as height of video
     canvas.height = $("#video").height();
+
+    //apply same height and width to the canvas container div
+    $(".canvascontainer").width(canvas.width).height(canvas.height);
+    
+    //apply same filter applied to video to canvas element
+    context.filter = $("#video").css('filter');
     
     //draw the image on canvas
     //image to draw is in element with id video
     context.drawImage(video,0,0,canvas.width,canvas.height);
+     
   });
