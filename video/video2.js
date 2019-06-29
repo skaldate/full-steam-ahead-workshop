eecb065fbb9dbed62a2a2b11012e604a397088c3
Shradha Kaldate <shradha.kaldate@gmail.com>
2019-06-28 22:30:24 -0400

    Update video2.js

--- video/video2.js
+++ video/video2.js
@@ -1,26 +1,29 @@
+
 $(document).ready(function(){
   //get the video element created in index.html file
   const videoElement = document.getElementById("video");
-  
+  let facingMode = 'user';
+
   //when button with id start is clicked
   $("#start").click(function(){
-      startVideo(videoElement);
+      startVideo(videoElement,facingMode);
   });
 
   //when button with id stop is clicked
   $("#stop").click(function(){
-      stopVideo(videoElement);
+    stopVideo(videoElement);
   });
-  
+   
   //Add the code to change facingMode
+
 
 });
 
-function startVideo(videoElement){
+function startVideo(videoElement,facingMode){
     //get media on users device, select video
       navigator.mediaDevices.getUserMedia(
         //then get the video stream from device's video camera 
-        { video: { facingMode: 'user' }
+        { video: { facingMode: facingMode}
       })
       .then(stream => {
         //assign the stream to video html element's src object. 
@@ -33,12 +36,12 @@ function startVideo(videoElement){
       });
 };
 
-function stopVideo(videoElement, videoStream){
+function stopVideo(videoElement){
   //if the video is playing
     //i:e videoStream is not empty.
-    let videoStream = videoElement.srcObject;
-    if(videoStream != undefined){
+    let videoStream1 = videoElement.srcObject;
+    if(videoStream1 != undefined){
       //stop the video
-      videoStream.getTracks()[0].stop();
+      videoStream1.getTracks()[0].stop();
     }
 };
